2db8fc3815e151cceea1485005b0e94e0a7e9227
Ben Field <fieldben97@gmail.com>
2023-11-10 18:15:42 +1100

    delted junk

--- ripe.py
+++ ripe.py
@@ -1,55 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-
-# # Title of the web application
-# st.title('Excel File Upload and Analysis')
-
-# # File uploader allows user to add their own Excel file
-# uploaded_file = st.file_uploader("Upload your Excel file", type=["xlsx"])
-
-# # Function to format description with <br> tags
-# def format_description(description):
-#     # Replace each newline character with <br><br> and avoid duplicates
-#     return '<br><br>'.join(filter(None, description.split('\n')))
-
-# # Function to strip leading bullet points from dot points
-# def strip_bullet_points(text):
-#     # Define characters to strip from the beginning of the string
-#     characters_to_strip = '•*- '
-#     return text.lstrip(characters_to_strip)
-
-# # Function to generate the HTML structure from a row of dataframe
-# def generate_html(row_data):
-#     description = format_description(row_data['Description'])
-#     html_template = f"""
-#     {description}
-#     <br><br>
-#     <ul>
-#         <li>{strip_bullet_points(row_data['Dot Point 2'])}</li>
-#         <li>{strip_bullet_points(row_data['Dot Point 3'])}</li>
-#         <li>{strip_bullet_points(row_data['Dot Point 5'])}</li>
-#         <li>{strip_bullet_points(row_data['Dot Point 6'])}</li>
-#         <li>{strip_bullet_points(row_data['Dot Point 7'])}</li>
-#         <li>{strip_bullet_points(row_data['Dot Point 8'])}</li>
-#         <li>{strip_bullet_points(row_data['Dot Point 4'])}</li>
-#         <li>{strip_bullet_points(row_data['Dot Point 1'])}</li>
-#     </ul>
-#     """
-#     return html_template.strip()
-
-# # The Excel file is read into a dataframe and displayed
-# if uploaded_file is not None:
-#     # Convert the uploaded Excel file to a dataframe
-#     df = pd.read_excel(uploaded_file)
-
-#     # Convert dataframe rows to dictionaries and generate HTML for one row as an example
-#     rows = df.to_dict('records')
-#     example_html_content = generate_html(rows[0])  # assuming the Excel has the necessary columns
-#     st.write(example_html_content, unsafe_allow_html=True)
-
-#     # Other actions with the dataframe can be added here
-
-
 import streamlit as st
 import pandas as pd
 import openai
@@ -142,7 +90,7 @@ def generate_description(product_name, colour_code, colour_name, dp_1, dp_2, dp_
     {product_name} 	{colour_code}	{colour_name}
     {dp_1}	{dp_2}	{dp_3}	{dp_4} 	{dp_5}	{dp_6} 	{dp_7}  {dp_8}
     """
-    
+
     return chat_with_gpt4(desc_prompt)
 
 def format_description(description):
