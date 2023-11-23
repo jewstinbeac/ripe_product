8aa6b42f8f02b7e31c4e860da68a65d986e00065
jewstinbeac <102846792+jewstinbeac@users.noreply.github.com>
2023-11-23 14:20:11 +1100

    Update ripe.py

--- ripe.py
+++ ripe.py
@@ -195,14 +195,14 @@ def process_row(row, style_descriptions, style_htmls, alt_style_descriptions, al
     product_name = row['Product Name']
     colour_code = row['Colour Code']
     colour_name = row['Colour Name']
-    dp_1 = row['Dot Point 1'].fillna('')
-    dp_2 = row['Dot Point 2'].fillna('')
-    dp_3 = row['Dot Point 3'].fillna('')
-    dp_4 = row['Dot Point 4'].fillna('')
-    dp_5 = row['Dot Point 5'].fillna('')
-    dp_6 = row['Dot Point 6'].fillna('')
-    dp_7 = row['Dot Point 7'].fillna('')
-    dp_8 = row['Dot Point 8'].fillna('')
+    dp_1 = row['Dot Point 1']
+    dp_2 = row['Dot Point 2']
+    dp_3 = row['Dot Point 3']
+    dp_4 = row['Dot Point 4']
+    dp_5 = row['Dot Point 5']
+    dp_6 = row['Dot Point 6']
+    dp_7 = row['Dot Point 7']
+    dp_8 = row['Dot Point 8']
 
     point_list = [dp_1,dp_2, dp_3, dp_4, dp_5, dp_6, dp_7, dp_8]
     print(point_list)
@@ -266,7 +266,7 @@ if uploaded_file is not None:
         submit_button = st.form_submit_button(label='Do it :)')
 
     if submit_button:
-        df = pd.read_excel(workbook, sheet_name=sheet_name)
+        df = pd.read_excel(workbook, sheet_name=sheet_name, na_filter=False)
         descriptions, htmls, alt_descriptions, alt_htmls = process_dataframe(df)
 
         data = {'Generated Descriptions': descriptions, 'Generated HTMLs': htmls, 'Alternative Descriptions': alt_descriptions, 'Alternative HTMLs': alt_htmls}
